610d8a670ed17c07463a8b131d09715eb28fb95d
Bunkang.Lee <liwenhansj@163.com>
2018-09-18 11:02:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# webpack-config-ok20180918
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist/js/')
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: "style-loader" }, // creates style nodes from JS strings
+                    { loader: "css-loader", options: { importLoaders: 1 } }, // translates CSS into CommonJS
+                    { loader: 'postcss-loader' },
+                    { loader: "sass-loader" } // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+
+
+        ]
+    }
+};
